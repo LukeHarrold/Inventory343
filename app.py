8b0c7d811bb1a690a0b47e7a39c6101ab905dacd
sam1360 <sam1360@g.rit.edu>
2017-03-13 14:28:37 -0400

    Initial creations for the database

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from __init__ import app, db
+import configparser
+
+config = configparser.ConfigParser()
+configValues = config.read("config.ini")
+if len(configValues) < 1:
+    # Will add aditional items to the config file as neeeded.
+    config['swen-343-database'] = {}
+    config['swen-343-database']['uri'] = input('Enter the DB URI: ')
+    # config['swen-343-server'] = {}
+    # config['swen-343-server']['admin'] = {}
+    # config['swen-343-server']['admin']['username'] = input('Enter the desired server admin username:')
+    # config['swen-343-server']['admin']['password'] = input('Enter the desired server admin password:')
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+# app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config['swen-343-database']['uri']
+# db = SQLAlchemy(app)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()

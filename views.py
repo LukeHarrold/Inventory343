73e20bb9a3e59d97793c1348010d1db00b2993cd
sam1360 <sam1360@g.rit.edu>
2017-03-24 17:57:19 -0400

    Request test functionality

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from __init__ import app, db
 from flask import request
 from flask import render_template
+import requests
 
 import json
 
@@ -24,6 +25,20 @@ def receive_completed_phones():
 
 
 @app.route('/inventory/phones/ordermock', methods=['POST'])
+def phone_orders_mock():
+    orderData = request.get_json(force=True)
+    print(orderData)
+    r = requests.post('http://127.0.0.1:5000/inventory/phones/order', data = json.dumps({"key": "test data"}))
+    print(r)
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+
+@app.route('/inventory/phones/order', methods=['POST'])
 def phone_orders():
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    print("HEYYYYYOOOOOO")
+    data = request.get_json(force=True)
+    print(data)
+
+    return app.make_response("junk")
+
 

74969fb3cfac72683170e71d3d18fbff27cc69e7
Samuel Mosher <sam1360@users.noreply.github.com>
2017-05-07 19:45:02 -0400

    Fixed the sql setup (#12)
    * Created DB setup script. Started another call.
    
    * Combined db into model.py
    
    * More changes to the db
    
    * changes to model.py
    
    * Merge of master
    
    * Merge
    
    * Changed the setup to make phones and parts match
    
    * Fixed up and added bogo columns for the setup sql
    
    * Change model to models, change a thing in views, modify setup.sql
    
    * Forgot to save or something
    
    * Added bogo
    
    * Add isRefurbished to the models and the db setup scripts
    
    * Changes to setup script to make it work properly

--- setup.sql
+++ setup.sql
@@ -1,49 +1,47 @@
 -- setup part types
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('screenHigh', 5.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('screenMedium', 4.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('screenLow', 3.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('screenFlip', 2.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('screenRadiation', 100.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('batteryHigh', 5.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('batteryMedium', 4.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('batteryLow', 3.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('batteryRadiation', 120.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('memory', 5.00, datetime('now'), NULL, NULL);
-INSERT INTO `part_types` (`partName`, `price`, `startDate`, `endDate`, `deletedAt`) VALUES ('memoryRadiation', 150.00, datetime('now'), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('screenHigh', 5.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('screenMedium', 4.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('screenLow', 3.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('screenFlip', 2.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('screenRadiation', 100.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('batteryHigh', 5.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('batteryMedium', 4.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('batteryLow', 3.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('batteryRadiation', 120.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('memory', 5.00, NOW(), NULL, NULL);
+INSERT INTO part_types (partName, price, startDate, endDate, deletedAt) VALUES ('memoryRadiation', 150.00, NOW(), NULL, NULL);
 
 -- setup phone types
-INSERT INTO `phone_types` (`phoneType`, `screenTypeId`, `batteryTypeId`, `memoryTypeId`, `description`, `imagePath`, `price`, `deletedAt`) VALUES ('High', 1, 6, 10, 'Top Tier Phone', 'static/images/high.png', 600.00, NULL);
-INSERT INTO `phone_types` (`phoneType`, `screenTypeId`, `batteryTypeId`, `memoryTypeId`, `description`, `imagePath`, `price`, `deletedAt`) VALUES ('Medium', 2, 7, 10, 'Medium Tier Phone', 'static/images/medium.png', 500.00, NULL);
-INSERT INTO `phone_types` (`phoneType`, `screenTypeId`, `batteryTypeId`, `memoryTypeId`, `description`, `imagePath`, `price`, `deletedAt`) VALUES ('Low', 3, 8, 10, 'Low Tier Phone', 'static/images/low.png', 300.00, NULL);
-INSERT INTO `phone_types` (`phoneType`, `screenTypeId`, `batteryTypeId`, `memoryTypeId`, `description`, `imagePath`, `price`, `deletedAt`) VALUES ('Retro', 4, 8, 10, 'Retro Flip Phone for those balling on a budget.', 'static/images/retro.png', 40.00, NULL);
-INSERT INTO `phone_types` (`phoneType`, `screenTypeId`, `batteryTypeId`, `memoryTypeId`, `description`, `imagePath`, `price`, `deletedAt`) VALUES ('Radiation King', 5, 9, 11, 'Radiation King: The Holographic Car Phone Model, now including a geiger counter.', 'static/images/radiation.png', 1000.00, NULL);
-
--- setup parts
---Phone id 1 parts high end
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (1, 0, 1, 1, 1, 0, 0);
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (1, 0, 1, 4, 1, 0, 0);
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (1, 0, 1, 7, 1, 0, 0);
-
---Phone id 2 parts with defective part middle end
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (2, 0, 1, 5, 3, 0, 0);
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (2, 1, 1, 2, 3, 0, 0);
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (2, 0, 1, 7, 3, 0, 0);
-
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (1, 0, 0, 5, 1, 0, 0);
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (5, 0, 0, 2, 4, 0, 0);
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (3, 0, 0, 7, 8, 0, 0);
-
-
---Phone id 3 refurbished with broken part in db
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (2, 0, 1, 5, 2, 0, 0);
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (2, 0, 1, 2, 2, 0, 0);
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (2, 0, 1, 7, 2, 0, 0);
-
-INSERT INTO `parts` (`modelType`, `defective`, `used`, `partTypeId`, `phoneId`, `bogo`, `isRecalled`) VALUES (2, 1, 0, 2, 2, 0, 0);
+INSERT INTO phone_types (phoneType, screenTypeId, batteryTypeId, memoryTypeId, description, imagePath, price, deletedAt) VALUES ('High', 1, 6, 10, 'Top Tier Phone', 'static/images/high.png', 600.00, NULL);
+INSERT INTO phone_types (phoneType, screenTypeId, batteryTypeId, memoryTypeId, description, imagePath, price, deletedAt) VALUES ('Medium', 2, 7, 10, 'Medium Tier Phone', 'static/images/medium.png', 500.00, NULL);
+INSERT INTO phone_types (phoneType, screenTypeId, batteryTypeId, memoryTypeId, description, imagePath, price, deletedAt) VALUES ('Low', 3, 8, 10, 'Low Tier Phone', 'static/images/low.png', 300.00, NULL);
+INSERT INTO phone_types (phoneType, screenTypeId, batteryTypeId, memoryTypeId, description, imagePath, price, deletedAt) VALUES ('Retro', 4, 8, 10, 'Retro Flip Phone for those balling on a budget.', 'static/images/retro.png', 40.00, NULL);
+INSERT INTO phone_types (phoneType, screenTypeId, batteryTypeId, memoryTypeId, description, imagePath, price, deletedAt) VALUES ('Radiation King', 5, 9, 11, 'Radiation King: The Holographic Car Phone Model, now including a geiger counter.', 'static/images/radiation.png', 1000.00, NULL);
 
 -- setup phones
-INSERT INTO `phones` (`status`,`modelId`,`saleDate`,`returnDate`,`refurbishedDate`, `bogo`, `isRecalled`) VALUES ('New', 1, datetime('now'), datetime('now'), datetime('now'),0, 0);
-INSERT INTO `phones` (`status`,`modelId`,`saleDate`,`returnDate`,`refurbishedDate`, `bogo`, `isRecalled`) VALUES ('Broken', 2, datetime('now'), datetime('now'), datetime('now'), 0, 0);
-INSERT INTO `phones` (`status`,`modelId`,`saleDate`,`returnDate`,`refurbishedDate`, `bogo`, `isRecalled`) VALUES ('Refurbished', 2, datetime('now'), datetime('now'), datetime('now'), 0, 0);
-INSERT INTO `phones` (`status`,`modelId`,`saleDate`,`returnDate`,`refurbishedDate`, `bogo`, `isRecalled`) VALUES ('New', 2, null, null, datetime('now'), 0, 0);
-INSERT INTO `phones` (`status`,`modelId`,`saleDate`,`returnDate`,`refurbishedDate`, `bogo`, `isRecalled`) VALUES ('Refurbished', 2, datetime('now'), null, datetime('now'), 0, 0);
+INSERT INTO phones (status,modelId,saleDate,returnDate,refurbishedDate, bogo) VALUES ('New', 1, NOW(), null, null,0);
+INSERT INTO phones (status,modelId,saleDate,returnDate,refurbishedDate, bogo) VALUES ('Broken', 2, NOW(), NOW(), null, 0);
+INSERT INTO phones (status,modelId,saleDate,returnDate,refurbishedDate, bogo) VALUES ('Refurbished', 2, NOW(), NOW(), NOW(), 0);
+INSERT INTO phones (status,modelId,saleDate,returnDate,refurbishedDate, bogo) VALUES ('New', 2, NOW(), null, null, 0);
+INSERT INTO phones (status,modelId,saleDate,returnDate,refurbishedDate, bogo) VALUES ('Refurbished', 2, null, NOW(), NOW(), 0);
+
+
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (1, 0, 1, 1, 1, 0, 0);
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (1, 0, 1, 4, 1, 0, 0);
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (1, 0, 1, 7, 1, 0, 0);
+
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (2, 0, 1, 5, 3, 0, 0);
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (2, 1, 1, 2, 3, 0, 0);
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (2, 0, 1, 7, 3, 0, 0);
+
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (1, 0, 0, 5, 1, 0, 0);
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (5, 0, 0, 2, 4, 0, 0);
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (3, 0, 0, 7, 8, 0, 0);
+
+
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (2, 0, 1, 5, 2, 0, 0);
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (2, 0, 1, 2, 2, 0, 0);
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (2, 0, 1, 7, 2, 0, 0);
+
+INSERT INTO parts (modelType, defective, used, partTypeId, phoneId, bogo, isRecalled) VALUES (2, 1, 0, 2, 2, 0, 0);
+
